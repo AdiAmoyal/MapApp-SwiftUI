ecf27fadc3d39794cfd24bee95f27765050f1ea9
AdiAmoyal <adi.amoyal02@gmail.com>
2024-12-27 11:26:15 -0800

    Initial Commit

---
+++ MapApp-SwiftUI/MapApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp_SwiftUIApp.swift
+//  MapApp-SwiftUI
+//
+//  Created by Adi Amoyal on 27/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
